8668e7cbfed457c4539e4a21799b01c8b09d4d34
Anna Shipilova <anyashi2002@gmail.com>
2018-08-17 18:43:25 +0300

    More actions were written in methods in Presenter for betten MVP

--- app/src/main/java/com/example/dima/robodoc/domain/form/FormContract.java
+++ app/src/main/java/com/example/dima/robodoc/domain/form/FormContract.java
@@ -1,10 +1,19 @@
 package com.example.dima.robodoc.domain.form;
 
 
+import android.widget.EditText;
+
+import com.example.dima.robodoc.data.models.Blood;
+import com.example.dima.robodoc.data.models.Disease;
+import com.example.dima.robodoc.data.models.Patient;
+
+import java.util.ArrayList;
 
 public interface FormContract {
     interface View {
         void setGender();
+
+        void transmitValues(Patient patient);
 
 
 
@@ -15,5 +24,11 @@ public interface FormContract {
         String createName(String hint);
 
         boolean checkName(String name);
+
+        boolean checkFields(EditText[] editTexts);
+
+        ArrayList<Blood> createBloodArrayList(EditText [] editTexts);
+
+        Patient createPatient(Patient patient, Blood blood, ArrayList<Disease> diseases);
     }
 }
--- app/src/main/java/com/example/dima/robodoc/domain/form/FormPresenter.java
+++ app/src/main/java/com/example/dima/robodoc/domain/form/FormPresenter.java
@@ -1,6 +1,14 @@
 package com.example.dima.robodoc.domain.form;
 
 
+import android.widget.EditText;
+
+import com.example.dima.robodoc.data.models.Blood;
+import com.example.dima.robodoc.data.models.Disease;
+import com.example.dima.robodoc.data.models.Patient;
+
+import java.util.ArrayList;
+
 public class FormPresenter implements FormContract.Presenter{
 
     @Override
@@ -13,6 +21,49 @@ public class FormPresenter implements FormContract.Presenter{
 
     }
 
+    @Override
+    public boolean checkFields(EditText[] editTexts) {
+        boolean check = true;
+        for (EditText temp : editTexts) {
+            if (temp.getText().toString().trim().length() != 0) {
+                check = false;
+            }
+        }
+        return check;
+    }
+
+    @Override
+    public ArrayList<Blood> createBloodArrayList(EditText [] editTexts) {
+        ArrayList<Blood> bloodArrayList = new ArrayList<>();
+        for (EditText temp : editTexts) {
+            if (temp.getText().length() > 0) {
+                String name = createName(temp.getHint().toString());
+                bloodArrayList.add(new Blood(name.trim(), Double.valueOf(temp.getText().toString())));
+            }
+        }
+        return bloodArrayList;
+
+    }
+
+    @Override
+    public Patient createPatient(Patient patient, Blood blood, ArrayList<Disease> diseases) {
+        patient.setState(true);
+        for(Boolean temp : blood.getNorma()){
+            if(!temp){
+                patient.setState(false);
+            }
+        }
+
+        if (!patient.isState()) {
+            if (diseases.size() != 0) {
+                patient.setDiseases(diseases);
+            } else {
+                patient.setDiseases(new ArrayList<Disease>());
+            }
+        }
+        return patient;
+
+    }
     @Override
     public String createName(String hint) {
 
