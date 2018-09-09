27cec1fad2cc6ccf2e338cafe39ccaaeaf2bc3a5
Anna Shipilova <anyashi2002@gmail.com>
2018-09-09 14:48:35 +0300

    Added medicines for every disease

--- app/src/main/java/com/example/dima/robodoc/domain/MedicinesActivity.java
+++ app/src/main/java/com/example/dima/robodoc/domain/MedicinesActivity.java
@@ -6,14 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.dima.robodoc.R;
+import com.example.dima.robodoc.data.models.Disease;
 import com.example.dima.robodoc.data.models.Patient;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.RealmList;
 
 public class MedicinesActivity extends AppCompatActivity {
     private TextView warning, medicines;
     private Button button;
     private Patient patient;
+    private Realm realm;
+    private long id;
 
 
     @Override
@@ -26,7 +34,36 @@ public class MedicinesActivity extends AppCompatActivity {
         medicines = findViewById(R.id.medicines);
         button = findViewById(R.id.buttonMapActivity);
 
-        warning.setText(R.string.warning);
+        id = getIntent().getLongExtra("id", 0);
+        RealmConfiguration configFirst = new RealmConfiguration.Builder().name("firstrealm.realm").build();
+        realm = Realm.getInstance(configFirst);
+
+        try {
+            patient = realm.where(Patient.class).equalTo("id", id).findFirst();
+        } catch (Exception e) {
+            Toast.makeText(this, "Помилка", Toast.LENGTH_SHORT).show();
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        RealmList<Disease> diseases = patient.getDiseases();
+        for (Disease temp : diseases) {
+            RealmList<String> medicinesRealmList = temp.getMedicinesRealmList();
+            StringBuilder medicinesStringBuilder = new StringBuilder();
+            for (String medicinesTemp : medicinesRealmList)
+                medicinesStringBuilder.append(medicinesTemp.toLowerCase() + ", ");
+            char[] medicinesChars = medicinesStringBuilder.toString().toCharArray();
+            String medicinesFinal = "";
+            for (int i = 0; i < medicinesChars.length - 2; i++) {
+                medicinesFinal += medicinesChars[i];
+            }
+            if (temp.getMedicinesRealmList().size() > 0) {
+                stringBuilder.append(temp.getName() + ": " + medicinesFinal);
+                stringBuilder.append("\n");
+            }
+        }
+
+        medicines.setText(stringBuilder.toString());
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/dima/robodoc/utils/DiseaseDeterminant.java
+++ app/src/main/java/com/example/dima/robodoc/utils/DiseaseDeterminant.java
@@ -16,51 +16,66 @@ public class DiseaseDeterminant {
         this.context = context;
 
         RealmList<Disease> diseases = new RealmList<>();
-        RealmList<String> medicines = new RealmList<>();
+        RealmList<String> medicines;
 
         if (blood.isHBUp()) {
+            medicines = new RealmList<>();
             medicines.add("Регідрон");
             diseases.add(new Disease("Зневоднення", R.drawable.dehydration, medicines));
         }
         if (blood.isHBUp() && blood.isRBCUp()) {
+            medicines = new RealmList<>();
             medicines.add("Гепарін");
             diseases.add(new Disease("Згущення крові", R.drawable.blood_clotting, medicines));
         }
         if (blood.isMCHCUp()) {
+            medicines = new RealmList<>();
             medicines.add("Вітаміни");
+            diseases.add(new Disease("Недостаток вітамінів", R.drawable.vitamins, medicines));
+            medicines = new RealmList<>();
             medicines.add("Белластезин");
             medicines.add("Еспумізан");
-            diseases.add(new Disease("Недостаток вітамінів", R.drawable.vitamins, medicines));
             diseases.add(new Disease("Проблеми з шлунком", R.drawable.stomach, medicines));
         }
         if (blood.isMCHCDown() && blood.isRTCDown() && blood.isPLTDown()
                 && blood.isWBCDown() && blood.isLYMDown() && blood.isMONDown()
                 && blood.isHBDown() && blood.isESRUp()) {
+            medicines = new RealmList<>();
             medicines.add("Актиферрин");
             diseases.add(new Disease("Анемія", R.drawable.anemia, medicines));
         }
-        if (blood.isWBCUp() && blood.isMCHCUp() && blood.isRBCUp())
+        if (blood.isWBCUp() && blood.isMCHCUp() && blood.isRBCUp()) {
+            medicines = new RealmList<>();
             diseases.add(new Disease("Новоутворення", R.drawable.moles, medicines));
+        }
         if (blood.isRTCDown() && blood.isRBCUp() && blood.isESRUp()) {
+            medicines = new RealmList<>();
             medicines.add("Уролесан");
             medicines.add("Канефрон");
             diseases.add(new Disease("Захворювання нирок", R.drawable.kidneys, medicines));
         }
-        if (blood.isRTCUp() && blood.isRBCUp())
+        if (blood.isRTCUp() && blood.isRBCUp()) {
+            medicines = new RealmList<>();
             diseases.add(new Disease("Крововтрата", R.drawable.hemorrhage, medicines));
+        }
         if (blood.isMONDown() || blood.isMONUp()) {
             if (blood.isPLTUp() && blood.isLYMUp() && blood.isEOSUp() && blood.isWBCDown()) {
+                medicines = new RealmList<>();
                 diseases.add(new Disease("Лейкоз", R.drawable.leukemia, medicines));
             }
         }
 
         if (blood.isLYMUp() && blood.isMONUp() && blood.isWBCDown()) {
+            medicines = new RealmList<>();
             medicines.add("Афлубін");
             diseases.add(new Disease("Вірусне захворювання", R.drawable.infection, medicines));
         }
-        if (blood.isESRUp() && blood.isMONUp() && blood.isEOSUp() && blood.isPLTDown() && blood.isLYMDown())
+        if (blood.isESRUp() && blood.isMONUp() && blood.isEOSUp() && blood.isPLTDown() && blood.isLYMDown()) {
+            medicines = new RealmList<>();
             diseases.add(new Disease("Ауто-імунні захворювання", R.drawable.autoimmune, medicines));
+        }
         if (blood.isESRUp()) {
+            medicines = new RealmList<>();
             medicines.add("Галстена");
             medicines.add("Карсил");
             medicines.add("Енсеале");
@@ -68,10 +83,12 @@ public class DiseaseDeterminant {
         }
         if (blood.isESRUp() && blood.isPLTUp() && blood.isWBCUp() && blood.isMONUp()
                 && blood.isLYMDown() && blood.isEOSDown()) {
+            medicines = new RealmList<>();
             medicines.add("Антибіотики");
             diseases.add(new Disease("Інфекційне захворювання", R.drawable.infection, medicines));
         }
         if (blood.isWBCDown()) {
+            medicines = new RealmList<>();
             medicines.add("Бисептазол");
             diseases.add(new Disease("Тиф", R.drawable.typhus, medicines));
         }
